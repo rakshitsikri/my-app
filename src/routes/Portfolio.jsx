4a9c2e74dfe4b7becc74dcd11b52e1f7117a1829
rakshit <rakshitsikri24@gmail.com>
2023-11-24 09:36:00 +0530

    design

--- src/routes/Portfolio.jsx
+++ src/routes/Portfolio.jsx
@@ -34,49 +34,49 @@ const Portfolio = () => {
                         <div className='project_name text-center'>
                             <a href="/" className='portfolio_name'>
                                 <span>Museo Camera</span>
-                                <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 20}px, -${position.y / 20}px)`,}}>
+                                <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 10}px, -${position.y / 10}px)`,}}>
                                     <img src='https://www.indiainternets.com/images/image-portfolio/1875737343_museo-thamb.jpg' />
                                 </div>
                             </a>
                             <a href="/" className='portfolio_name'>
                                 <span>Forms Design Studio</span>
-                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 20}px, -${position.y / 20}px)`,}}>
+                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 10}px, -${position.y / 10}px)`,}}>
                                         <img src='https://www.indiainternets.com/images/image-portfolio/1875737343_museo-thamb.jpg' />
                                     </div>
                                 </a>
                             <a href="/" className='portfolio_name'>
                                 <span>IOC Phinergy</span>
-                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 20}px, -${position.y / 20}px)`,}}>
+                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 10}px, -${position.y / 10}px)`,}}>
                                         <img src='https://www.indiainternets.com/images/image-portfolio/1875737343_museo-thamb.jpg' />
                                     </div>
                                 </a>
                             <a href="/" className='portfolio_name'>
                                 <span>Prop Bridge</span>
-                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 20}px, -${position.y / 20}px)`,}}>
+                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 10}px, -${position.y / 10}px)`,}}>
                                         <img src='https://www.indiainternets.com/images/image-portfolio/1875737343_museo-thamb.jpg' />
                                     </div>
                                 </a>
                             <a href="/" className='portfolio_name'>
                                 <span>Rise International</span>
-                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 20}px, -${position.y / 20}px)`,}}>
+                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 10}px, -${position.y / 10}px)`,}}>
                                         <img src='https://www.indiainternets.com/images/image-portfolio/1875737343_museo-thamb.jpg' />
                                     </div>
                                 </a>
                             <a href="/" className='portfolio_name'>
                                 <span>NPI Group</span>
-                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 20}px, -${position.y / 20}px)`,}}>
+                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 10}px, -${position.y / 10}px)`,}}>
                                         <img src='https://www.indiainternets.com/images/image-portfolio/1875737343_museo-thamb.jpg' />
                                     </div>
                                 </a>
                             <a href="/" className='portfolio_name'>
                                 <span>Riglam School</span>
-                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 20}px, -${position.y / 20}px)`,}}>
+                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 10}px, -${position.y / 10}px)`,}}>
                                         <img src='https://www.indiainternets.com/images/image-portfolio/1875737343_museo-thamb.jpg' />
                                     </div>
                                 </a>
                             <a href="/" className='portfolio_name'>
                                 <span>Dreamscape</span>
-                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 20}px, -${position.y / 20}px)`,}}>
+                                    <div className='portfolio_img parallax-content' style={{ transform: `translate(-${position.x / 10}px, -${position.y / 10}px)`,}}>
                                         <img src='https://www.indiainternets.com/images/image-portfolio/1875737343_museo-thamb.jpg' />
                                     </div>
                                 </a>
